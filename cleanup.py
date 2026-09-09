6c762dd6db6592cddbd5d6bfb9186e1da7ca69eb
Guilherme Augusto <guilherme.geremia@aluno.senai.br>
2026-09-08 21:26:09 -0300

    macunba tinah que funcianr

--- cleanup.py
+++ cleanup.py
@@ -1,21 +1,58 @@
+import sys
 import json
 import subprocess
-import sys
 from datetime import datetime, timezone
 
-bucket, endpoint, cutoff = sys.argv[1], sys.argv[2], int(sys.argv[3])
-items = json.load(sys.stdin)
 
-for item in items or []:
-    key = item["Key"]
-    modified = item["LastModified"]
-    dt = datetime.fromisoformat(modified.replace("Z", "+00:00"))
-    if dt.timestamp() < cutoff:
-        print(f"Removendo: {key}")
+bucket = sys.argv[1]
+endpoint = sys.argv[2]
+cutoff_text = sys.argv[3]
+
+# Converte a data recebida para datetime
+cutoff_text = cutoff_text.replace("Z", "+00:00")
+cutoff = datetime.fromisoformat(cutoff_text)
+
+if cutoff.tzinfo is None:
+    cutoff = cutoff.replace(tzinfo=timezone.utc)
+
+# Lê a resposta do aws s3api list-objects-v2
+data = json.load(sys.stdin)
+
+contents = data.get("Contents", [])
+
+deleted = 0
+
+for obj in contents:
+    key = obj.get("Key")
+    last_modified = obj.get("LastModified")
+
+    if not key or not last_modified:
+        continue
+
+    last_modified = last_modified.replace("Z", "+00:00")
+    modified = datetime.fromisoformat(last_modified)
+
+    if modified.tzinfo is None:
+        modified = modified.replace(tzinfo=timezone.utc)
+
+    if modified < cutoff:
+        print(f"Removendo backup antigo: {key}")
+
         subprocess.run(
             [
-                "aws", "s3", "rm", f"s3://{bucket}/{key}",
-                "--endpoint-url", endpoint,
+                "aws",
+                "s3api",
+                "delete-object",
+                "--bucket",
+                bucket,
+                "--key",
+                key,
+                "--endpoint-url",
+                endpoint,
             ],
             check=True,
         )
+
+        deleted += 1
+
+print(f"Limpeza concluída. Arquivos removidos: {deleted}")
